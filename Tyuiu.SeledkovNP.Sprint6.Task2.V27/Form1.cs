c2d098ddc2ed5d97f6e33a1475ccdf4f7512e481
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-12-09 10:27:51 +0500

    Task2

---
+++ Tyuiu.SeledkovNP.Sprint6.Task2.V27/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.SeledkovNP.Sprint6.Task2.V27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
