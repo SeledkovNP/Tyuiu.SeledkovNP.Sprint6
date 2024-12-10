26ff2281333aedbc32c7fe52ced33532fc5b74db
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-12-10 19:25:04 +0500

    Fix5-1

--- Tyuiu.SeledkovNP.Sprint6.Task6.V22.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint6.Task6.V22.Lib/DataService.cs
@@ -18,9 +18,11 @@ namespace Tyuiu.SeledkovNP.Sprint6.Task6.V22.Lib
                     while ((line = reader.ReadLine()) != null)
                     {
                     resStr += " " + line.Split(' ')[0];
+                    path = resStr;
+
                     }
                 }
-                return resStr;
+                return path;
 
 
             /*
